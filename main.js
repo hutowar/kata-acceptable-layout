606c9b9f819b5fedfd4d43750ad624afe8f8daaf
Yurii Bredchenko <hutowar1@gmail.com>
2024-09-19 18:34:10 +0300

    //

---
+++ main.js
@@ -0,0 +1,26 @@
+// SCROLL CATEGORY LIST
+const scrollableEl = document.querySelector(".category-list");
+const scrollableEls = document.querySelectorAll(".category-list a");
+let isDragging = false;
+const dragging = (e) => {
+  if (!isDragging) return;
+  scrollableEl.scrollLeft -= e.movementX;
+};
+scrollableEls.forEach((elem) => {
+  elem.addEventListener("mousedown", (e) => {
+    e.preventDefault();
+  });
+});
+scrollableEl.addEventListener("mousedown", () => (isDragging = true));
+scrollableEl.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", () => (isDragging = false));
+//ACTIVE CLASS CATEGORY LIST
+scrollableEls.forEach((el) => {
+  console.log(el.parentNode);
+  el.addEventListener("click", function () {
+    scrollableEls.forEach(element => {
+      element.parentNode.classList.remove("category-list__item--active");
+    });
+    el.parentNode.classList.add("category-list__item--active");
+  });
+});
